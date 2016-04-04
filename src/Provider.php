8282d3b29c511996345d4a561adf9190427da03d
DraperStudio <hello@draperstudio.tech>
2016-04-04 02:52:17 -0400

    Applied fixes from StyleCI

--- src/Provider.php
+++ src/Provider.php
@@ -51,7 +51,6 @@ class Provider extends AbstractProvider implements ProviderInterface
             'form_params' => $this->getTokenFields($code),
         ]);
 
-
         $this->credentialsResponseBody = json_decode($response->getBody(), true);
 
         return $this->parseAccessToken($response->getBody());
